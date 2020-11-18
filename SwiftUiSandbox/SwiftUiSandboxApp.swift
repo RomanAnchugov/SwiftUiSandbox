c4f8444e5739fc66a1c7e49a514bd50976adc524
Roman Anchugov <r.anchugov@rambler-co.ru>
2020-11-18 20:27:11 +0300

    Initial Commit

---
+++ SwiftUiSandbox/SwiftUiSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiSandboxApp.swift
+//  SwiftUiSandbox
+//
+//  Created by r.anchugov on 18.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
