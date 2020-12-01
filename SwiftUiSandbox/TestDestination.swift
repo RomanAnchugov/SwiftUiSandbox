52c44dcb2999581641b233b22333099566097051
Roman Anchugov <r.anchugov@rambler-co.ru>
2020-12-01 22:02:52 +0300

    first commit

---
+++ SwiftUiSandbox/TestDestination.swift
@@ -0,0 +1,20 @@
+//
+//  TestDestination.swift
+//  SwiftUiSandbox
+//
+//  Created by r.anchugov on 18.11.2020.
+//
+
+import SwiftUI
+
+struct TestDestination: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestDestination_Previews: PreviewProvider {
+    static var previews: some View {
+        TestDestination()
+    }
+}
---
+++ SwiftUiSandbox/TestObservableData.swift
@@ -0,0 +1,8 @@
+//
+//  TestObservableData.swift
+//  SwiftUiSandbox
+//
+//  Created by r.anchugov on 18.11.2020.
+//
+
+import Foundation
